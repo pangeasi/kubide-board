909513cc37b4d973ec4af620975c857fb7abe5d8
Dani <pangeanoteniafronteras@gmail.com>
2021-01-19 13:54:45 +0100

    show readme on root router

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { Converter } from 'showdown';
+import { readFileSync } from 'fs';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const converter = new Converter();
+    const readme = readFileSync(`${__dirname}/../README.md`, 'utf8');
+    return converter.makeHtml(readme);
   }
 }
